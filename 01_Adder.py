cc01f7e5df250f7397901f5c3511a309fa7b14af
JbBhetwal <jbjbjbaaa@gmail.com>
2024-05-26 19:15:42 +0545

    Adder and subtractor of unsigned binary numbers.

---
+++ 01_Adder.py
@@ -0,0 +1,44 @@
+# Create addition using full adder that takes two inputs in binary and provides output in binary
+
+def and_gate(a, b):
+    return a and b
+
+def or_gate(a, b):
+    return a or b
+
+def xor_gate(a, b):
+    return a ^ b
+
+def not_gate(a):
+    return not a
+
+def full_adder(a, b, c):
+    sum = xor_gate(xor_gate(a, b), c)
+    carry = or_gate(and_gate(a, b), and_gate(c, xor_gate(a, b)))
+    return sum, carry
+
+def binary_addition(A, B):
+    A=A[::-1]
+    B=B[::-1]
+    Cin=0
+    S=[]
+    for i in range(len(A)):
+        sum, Cin = full_adder(A[i], B[i], Cin)
+        S.append(sum)
+    S.append(Cin)
+    return S[::-1]
+
+
+num1=list(input("Enter 1st binary number: "))
+num2=list(input("Enter 2nd binary number: "))
+A=[]
+for i in range(len(num1)):
+    A.append(int(num1[i]))
+B=[]
+for i in range(len(num2)):
+    B.append(int(num2[i]))
+print(A)
+print(B)
+Sum=binary_addition(A, B)
+for i in range(len(Sum)):
+    print(Sum[i], end="")
---
+++ 02_Subtractor.py
@@ -0,0 +1,50 @@
+# Create addition using full subtractor that takes two inputs in binary and provides output in binary
+
+def and_gate(a, b):
+    return a and b
+
+def or_gate(a, b):
+    return a or b
+
+def xor_gate(a, b):
+    return a ^ b
+
+def not_gate(a):
+    return not a
+
+def full_adder(a, b, c):
+    sum = xor_gate(xor_gate(a, b), c)
+    carry = or_gate(and_gate(a, b), and_gate(c, xor_gate(a, b)))
+    return sum, carry
+
+def ones_complement(A):
+    for i in range(len(A)):
+        A[i]=not_gate(A[i])
+    return A
+
+def binary_subtraction(A, B):
+    A=A[::-1]
+    B=B[::-1]
+    B=ones_complement(B)
+    Cin=1
+    S=[]
+    for i in range(len(A)):
+        diff, Cin = full_adder(A[i], B[i], Cin)
+        S.append(diff)
+    S.append(Cin)
+    return S[::-1]
+
+
+num1=list(input("Enter 1st binary number: "))
+num2=list(input("Enter 2nd binary number: "))
+A=[]
+for i in range(len(num1)):
+    A.append(int(num1[i]))
+B=[]
+for i in range(len(num2)):
+    B.append(int(num2[i]))
+print(A)
+print(B)
+Sum=binary_subtraction(A, B)
+for i in range(len(Sum)):
+    print(Sum[i], end="")
